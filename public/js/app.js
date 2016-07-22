17776172cfbd9a66de69c25240a1a24ed3b2b179
PFletcher <PFletcher>
2016-07-22 19:10:11 -0400

    added angular route js

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,4 @@
 
-angular.module('ngWeather', []);
+angular.module('ngWeather', [
+  'ngRoute'
+]);
---
+++ public/js/routes.js
@@ -0,0 +1,29 @@
+
+
+(function(){
+  angular.module('ngWeather')
+        .config(RouteConfig);
+
+
+  RouteConfig.$inject = ['$routeProvider', '$locationProvider'];
+
+  function RouteConfig($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        template: 'home'
+      })
+      .when('/hourly', {
+        template: 'hourly'
+      })
+
+      .when('/minutely', {
+        template: 'minutely'
+      })
+
+      .when('/daily', {
+        template: 'daily'
+      });
+
+
+  }
+})();
