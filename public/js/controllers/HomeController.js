ceb464f63a2f52b295ea4b1e97c71c88d716fd49
PFletcher <PFletcher>
2016-07-22 21:04:32 -0400

    updated homecontrollers

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -2,9 +2,22 @@
   angular.module('ngWeather')
         .controller('HomeController', HomeController);
 
-  HomeController.$inject = [];
+  HomeController.$inject = ['$scope', 'WeatherService'];
 
-  function HomeController(){
-    
+  function HomeController($scope, WeatherService){
+    $scope.updateHourly = updateHourly;
+    $scope.updateMinutely = updateMinutely;
+    $scope.updateDaily = updateDaily;
+
+    function updateHourly(latitude, longitude){
+      WeatherService.getHourlyData(latitude, longitude);
+    }
+    function updateMinutely(latitude, longitude){
+      WeatherService.getMinutelyData(latitude, longitude);
+    }
+    function updateDaily(latitude, longitude){
+      WeatherService.getDailyData(latitude, longitude);
+    }
+
   }
 })();
