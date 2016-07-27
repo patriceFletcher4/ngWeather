395e7d01a912164839ff10baf43329148784f9aa
PFletcher <PFletcher>
2016-07-26 20:39:09 -0400

    implemented minutelyView

--- public/js/controllers/MinutelyController.js
+++ public/js/controllers/MinutelyController.js
@@ -2,9 +2,21 @@
   angular.module('ngWeather')
         .controller('MinutelyController', MinutelyController);
 
-  MinutelyController.$inject = [];
+  MinutelyController.$inject = ['$scope', 'WeatherService'];
 
-  function MinutelyController(){
+  function MinutelyController($scope, WeatherService){
+    $scope.minutelyData = WeatherService.weather;
+    $scope.summaryLookup = {
+      'Drizzle': 'There will be some drizzle',
+      'Partly Cloudy': 'There will be some clouds around in your area.'
+    };
+    $scope.$watch(function(){
+      return WeatherService.weather;
+    }, function(value){
+     $scope.minutelyData = value;
+
+    });
+
 
   }
 })();
